73296ef7c89dd2f4b2acee7f788b9cd72be21dbd
Israel Castro <israel.castro@gmail.com>
2020-10-04 23:22:12 -0500

    [Add] external webpack config

--- external/asdf.js
+++ external/asdf.js
@@ -1,9 +1,10 @@
 const path = require('path'); // Importa el módulo path de node (commonJS)
 
 module.exports = {
-	entry: './index.js',
+	entry: path.resolve(__dirname, 'index.js'),
+	mode: 'development',
 	output: {
-		path: path.resolve(__dirname),
+		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js'
 	}
 }
