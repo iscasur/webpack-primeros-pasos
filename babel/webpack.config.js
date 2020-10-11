c97eeb75c12492b5100661982c7aa0aee45c354f
Israel Castro <israel.castro@gmail.com>
2020-10-11 12:27:13 -0500

    [Add] babel

---
+++ babel/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path'); // Importa el módulo path de node (commonJS)
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin') // Importamos el plugin
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Webpack = require('webpack')
+module.exports = {
+	entry: {
+		home: path.resolve(__dirname, 'src/js/index.js'),
+	},
+	mode: 'development',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'js/[name].js'
+	},
+	devServer: {
+		hot: true,
+		open: true,
+		port: 9000,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: 'babel-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader' // Entiende que el códig puede tener CSS
+				]
+			}
+		]
+	},
+	plugins: [
+		new Webpack.HotModuleReplacementPlugin(),
+		new HtmlWebpackPlugin({
+			title: 'Webpack-dev-server'
+		})
+	]
+}
