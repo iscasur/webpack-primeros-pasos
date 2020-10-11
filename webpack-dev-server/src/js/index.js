18258b9105f8fe6db93fc131ce2e63b76498f1a2
Israel Castro <israel.castro@gmail.com>
2020-10-11 11:52:23 -0500

    [Add] webpack dev server with HMR

--- webpack-dev-server/src/js/index.js
+++ webpack-dev-server/src/js/index.js
@@ -1,3 +1,13 @@
 import '../css/index.css'
 
-document.body.innerHTML = '<p>Hola mundo desde Webpack! Home</p>'
+import text from './text'   // Importamos text del archivo text.js (se omite la extensión porque se espera por default un js)
+
+text()
+
+// En react y veu la modularización es automática
+if (module.hot) {
+  module.hot.accept('./text.js', function() {
+    console.log('He recargado en caliente')
+    text()
+  })
+}
--- webpack-dev-server/webpack.config.js
+++ webpack-dev-server/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path'); // Importa el módulo path de node (commonJS)
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin') // Importamos el plugin
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Webpack = require('webpack')
 module.exports = {
 	entry: {
 		home: path.resolve(__dirname, 'src/js/index.js'),
@@ -10,25 +11,26 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'js/[name].js'
 	},
+	devServer: {
+		hot: true,
+		open: true,
+		port: 9000,
+	},
 	module: {
 		rules: [
 			{
 				test: /\.css$/,
 				use: [
-					{
-						loader: MiniCSSExtractPlugin.loader
-					},
+					'style-loader',
 					'css-loader' // Entiende que el códig puede tener CSS
 				]
 			}
 		]
 	},
 	plugins: [
+		new Webpack.HotModuleReplacementPlugin(),
 		new HtmlWebpackPlugin({
-			title: 'Plugins'
-		}),
-		new MiniCSSExtractPlugin({
-			filename: 'css/[name].css'
+			title: 'Webpack-dev-server'
 		})
 	]
 }
