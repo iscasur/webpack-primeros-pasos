30e4a6b53aa8508cc3562054585f9fbe40454fb3
Israel Castro <israel.castro@gmail.com>
2020-10-12 21:34:48 -0500

    [Add] binaries files support (jpg, png, mp4)

---
+++ url-loader/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path'); // Importa el módulo path de node (commonJS)
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin') // Importamos el plugin
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+module.exports = {
+	entry: {
+		home: path.resolve(__dirname, 'src/js/index.js'),
+	},
+	mode: 'development',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'js/[name].js'
+	},
+	devServer: {
+		hot: true,
+		open: true,
+		port: 9000,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: 'babel-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader' // Entiende que el códig puede tener CSS
+				]
+			},
+			{
+				test: /\.jpg|png|gif|woff|eot|ttf|svg|mp4|webm$/,
+				use: {
+					loader: 'url-loader',
+					options: {
+						limit: 90000,
+					}
+				},
+			}
+		]
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new HtmlWebpackPlugin({
+			title: 'Webpack-dev-server',
+			template: path.resolve(__dirname, 'index.html'),
+		})
+	]
+}
